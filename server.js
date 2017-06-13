dbec236f9de8e86d87836a7e1f89b6054c4ac988
Zimzik <mrzimko@gmail.com>
2017-06-14 00:56:10 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+let express = require('express'),
+    app = express(),
+    jade = require('jade'),
+    path = require('path'),
+    bodyParser = require('body-parser');
+
+app.use(express.static('/app/'));
+app.set('views', './app/jade');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+let server = app.listen(3000, function() {
+  console.log("Server listening on port" + server.address().port);
+})
